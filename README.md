86089fa492ec2b69664173e6cf36952fcf4c78a2
seanrose <seanrose@alumni.stanford.edu>
2013-09-26 17:39:50 -0700

    init

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 box-view-box-content
 ====================
 
-Showing how to use the Box Content API with the Box View API 
+Showing how to use the Box Content API with the Box View API
+
+# Add Your Credentials
+
+In `settings.py`, update `VIEW_API_KEY` and `CONTENT_TOKEN` to be your own credentials. If you need a content token quickly, you can use: box-token-generator.herokuapp.com
---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, jsonify, request, redirect
+import requests
+import time
+import settings as s
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+
+    root_folder_files = get_folder_files()
+
+    return jsonify(root_folder_files)
+
+
+@app.route('/view')
+def view():
+
+    boxcloud_link = get_boxcloud_for_file(request.args.get('file_id'))
+
+    print boxcloud_link
+
+    payload = {'url': boxcloud_link}
+
+    documents_resource = '/documents'
+    url = s.VIEW_API_URL + documents_resource
+    auth = {'Authorization': 'Token {}'.format(s.VIEW_API_KEY)}
+
+    api_response = requests.post(url, headers=auth, data=payload).json()
+    document_id = api_response['id']
+
+    time.sleep(12)
+
+    payload = {'document_id': document_id}
+    sessions_resource = '/sessions'
+    url = s.VIEW_API_URL + sessions_resource
+
+    api_response = requests.post(url, headers=auth, data=payload).json()
+    session_id = api_response['id']
+
+    view_base_url = 'https://view-api.box.com/view/'
+    view_url = '{}{}'.format(view_base_url, session_id)
+
+    return redirect(view_url)
+
+
+def get_folder_items(folder_id=0):
+
+    folders_resource = '/folders/{}/items'.format(folder_id)
+    url = s.CONTENT_API_URL + folders_resource
+    auth = {'Authorization': 'Bearer {}'.format(s.CONTENT_TOKEN)}
+
+    api_response = requests.get(url, headers=auth).json()
+
+    return api_response
+
+
+def get_folder_files(folder_id=0):
+
+    folder_items = get_folder_items(folder_id)
+
+    folder_files = [
+        item for item in folder_items['entries'] if item['type'] == 'file'
+    ]
+
+    folder_items['entries'] = folder_files
+    folder_items['total_count'] = len(folder_files)
+
+    return folder_items
+
+
+def get_boxcloud_for_file(file_id):
+
+    files_resource = '/files/{}/content'.format(file_id)
+    url = s.CONTENT_API_URL + files_resource
+    auth = {'Authorization': 'Bearer {}'.format(s.CONTENT_TOKEN)}
+
+    api_response = requests.get(url, headers=auth, allow_redirects=False)
+    boxcloud_link = api_response.headers['Location']
+
+    return boxcloud_link
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
